00c95ca2df8b3ac465838dac112fe146aa8b8346
Saevald12 <saevald12@ru.is>
2016-10-11 17:03:59 +0000

    added main and test

---
+++ src/test/java/is/ru/StringCalculator/StringCalculatorTest.java
@@ -0,0 +1,15 @@
+package is.ru.StringCalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class StringCalculatorTest
+{
+    
+    @Test
+    public void testEmptyString()
+    {
+    	assertEquals(0, StringCalculator.add(""));
+    	
+    }
+}
