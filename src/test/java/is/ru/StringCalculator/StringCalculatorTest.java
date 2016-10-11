72f00ed09533e5319df50c6bd60b0e00e50303a5
Saevald12 <saevald12@ru.is>
2016-10-11 17:46:35 +0000

    added tests for 1 and 2 numbers

--- src/test/java/is/ru/StringCalculator/StringCalculatorTest.java
+++ src/test/java/is/ru/StringCalculator/StringCalculatorTest.java
@@ -10,6 +10,19 @@ public class StringCalculatorTest
     public void testEmptyString()
     {
     	assertEquals(0, StringCalculator.add(""));
-    	
     }
+
+    @Test
+    public void testOneNumber()
+    {
+    	assertEquals(1, StringCalculator.add("1"));
+    }
+
+    @Test
+    public void testTwoNumbers()
+    {
+    	assertEquals(3, StringCalculator.add("1,2"));
+    }
+
+	
 }
